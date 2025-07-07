04f60645875e4a4ae5241bcf44555dce4648d672
barez azad <barez.001159544@gmail.com>
2025-07-07 19:29:13 +0300

    Mysql added but database still not working

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ dotenv.config();
 const cors = require("cors");
 const connectDB = require("./db/db");
 const port = process.env.PORT;
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/", authRoutes);
 
 connectDB();
 
